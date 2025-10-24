6f3b70c2343dc3c0da5e1628e4ad02b3554a3256
Sébastien Guerrero <sebastien.guerrero@insa-lyon.fr>
2025-10-24 17:01:21 +0200

    Data dimension

--- src/pymodaq_plugins_daqmx/daq_viewer_plugins/plugins_1D/daq_1Dviewer_NIDAQmx.py
+++ src/pymodaq_plugins_daqmx/daq_viewer_plugins/plugins_1D/daq_1Dviewer_NIDAQmx.py
@@ -6,7 +6,7 @@ from pymodaq_plugins_daqmx.hardware.national_instruments.NIDAQmx_Viewer import D
 
 class DAQ_1DViewer_NIDAQmx(DAQ_NIDAQmx_Viewer):
     """
-    Plugin for a 0D data visualization & acquisition with various NI modules plugged in a NI cDAQ.
+    Plugin for bufferized 1D data visualization & acquisition with NI modules (plugged in NI cDAQ or NI-USB).
     """
     def __init__(self, parent=None, params_state=None):
         super().__init__(parent, params_state, control_type='1D')
